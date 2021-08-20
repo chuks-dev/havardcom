c26699a887b4e0646ba8c06c79d6ead4daad3bca
Chuks Emmanuel <chuksdevinfo@gmail.com>
2021-08-20 13:49:13 +0100

    update weather app

--- CLASS_OF_JUNE/JS/weather-app/js/script.js
+++ CLASS_OF_JUNE/JS/weather-app/js/script.js
@@ -1,21 +1,38 @@
-const weatherBtn = document.querySelector('#weather-btn');
+const body = document.querySelector('body');
+const loader = document.querySelector('#loader');
+const weather = document.querySelector('.weather');
 
-weatherBtn.addEventListener('click', () => {
+// body.addEventListener('load', () => {
+// 	console.log('body loaded');
+// });
+
+function onBodyLoad() {
+	getWeatherInfo();
+}
+
+function getWeatherInfo() {
 	navigator.geolocation.getCurrentPosition(position => {
 		let lat = position.coords.latitude;
 		let lon = position.coords.longitude;
-		console.log.log(lat, lon);
+		console.log(lat, lon);
 		fetch(
-			`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=71490b444071195229aeecab8a054cd5`,
+			`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=71490b444071195229aeecab8a054cd5`,
 		)
 			.then(response => {
+				loader.remove();
 				return response.json();
 			})
 			.then(data => {
-				console.log(data);
+				if (data.weather[0].main === 'Clear') {
+					weather.style.background = "url('../img/bg-sun.jpg')";
+					weather.style.backgroundSize = 'cover';
+					weather.style.backgroundRepeat = 'no-repeat';
+				}
 			})
 			.catch(err => {
+				loader.remove();
+				alert('Something went wrong, please try again');
 				console.log(err);
 			});
 	});
-});
+}
